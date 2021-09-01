0086ca06d8fa1903f8a2c56ba4a77f3e0a4b3e1d
root <root@DESKTOP-48183QL.localdomain>
2021-09-01 19:28:41 +0100

    new

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+alert('hello from the other side');
+
+import { add, minus } from './lib.js';
+
+console.log(add(5, 10));
+
+document.querySelectorAll('h1')[0].style.color = 'blue';
+
+document.querySelectorAll('h1')[0].style.borderRadius = '30px';
+
+document.querySelectorAll('h1')[0].style.border = '30px solid black';
